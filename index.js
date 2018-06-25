b6caa4b927f1b315eb90b8abd7ee5619fb2de76b
monthergholam@gmail.com <monthergholam@gmail.com>
2018-06-25 23:43:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function add(a,b){
   return a+b
 }
+//Second
+function subtracts(a,b){
+  return a-b
+}
+//Third
+function multiply(a,b){
+  return a*b
+}
+//Fourth
+function divide(a,b){
+  return a/b
+}
